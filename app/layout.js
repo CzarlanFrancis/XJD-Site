83a673e794808a94d8533dd2a80ae53a8c92d595
CzarlanFrancis <executive@xjdfinance.com>
2025-08-17 15:58:00 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
+      <head></head>
       <body className="antialiased">{children}</body>
     </html>
   );
